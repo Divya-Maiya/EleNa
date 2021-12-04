0cfa2f27afc14337dc37f9168138cce1f9955c74
divyamaiya <dmaiya@umass.edu>
2021-12-04 14:27:08 -0500

    Add get route routine

--- controller/controller.py
+++ controller/controller.py
@@ -1,3 +1,8 @@
+from algorithm.aStar import astar
+from algorithm.dijkstra import dijkstra
+from algorithm.bfs import bfs
+from utils.mapUtils import convert_path
+
 class Controller(object):
 
     def _init_(self):
@@ -5,4 +10,23 @@ class Controller(object):
 
     def set_model(self, model):
         self.model = model
+
+    def get_route(self, graph, start_node, dest_node, algorithm='AStar', limit=0, mode='min'):
+        if algorithm == 'AStar':
+            path = astar(graph, limit, mode)
+
+        elif algorithm == 'Dijkstra':
+            path = dijkstra(graph, limit, mode)
+
+        elif algorithm == 'BFS':
+            path = bfs(graph, limit, mode)
+
+        print(path)
+
+        path, path_data = convert_path(graph, path)
+        return {'path': path, 'path_data': path_data}
+
+
+
+
 
