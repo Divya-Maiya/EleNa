15c7e096660403545fecd61e0f069fe72b8c04ae
divyamaiya <dmaiya@umass.edu>
2021-12-04 16:38:43 -0500

    Add local test script

---
+++ test.py
@@ -0,0 +1,9 @@
+from utils.map_utils import get_map
+from utils.map_utils import load_map
+
+# apiKey = "AIzaSyBQIIBs6JaQjM3OViYfk_KpuKdnUGZTq-o"
+#
+# get_map("Boston", "Massachusetts", "USA", apiKey)
+
+g = load_map()
+print(g)
