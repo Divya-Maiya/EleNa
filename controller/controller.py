46315e8cd9603756691d3200d20e5c6933a87bcd
divyamaiya <dmaiya@umass.edu>
2021-12-04 13:38:19 -0500

    Initialize directories and MVC

---
+++ controller/controller.py
@@ -0,0 +1,8 @@
+class Controller(object):
+
+    def _init_(self):
+        self.model = None
+
+    def set_model(self, model):
+        self.model = model
+
---
+++ model/model.py
@@ -0,0 +1,64 @@
+class Model(object):
+
+    def _init_(self):
+        # self.modes = ['minimize', 'maximize']
+        self.source = None
+        self.destination = None
+        self.overhead = None
+        self.mode = None
+        # self.graph_projection = None
+        self.algorithm = None
+        # self.bbox = None
+        # self.impedence = None
+        # self.key = None
+
+    # setters
+    def set_source(self, s):
+        self.source = s
+
+    def set_destination(self, d):
+        self.destination = d
+
+    def set_mode(self, mode):
+        self.mode = mode
+
+    def set_overhead(self, o):
+        self.overhead = o
+
+    def set_algorithm(self, algorithm):
+        self.algorithm = algorithm
+
+    # def set_bbox(self, bb):
+    #     self.bbox = bb
+    #
+    # def set_graph_projection(self, graph_projection):
+    #     self.graph_projection = graph_projection
+    #
+    # def set_key(self, key):
+    #     self.key = key
+
+    # getters
+
+    # def get_graph_projection(self):
+    #     return self.graph_projection
+
+    def get_source(self):
+        return self.source
+
+    def get_destination(self):
+        return self.destination
+
+    def get_mode(self):
+        return self.mode
+
+    def get_overhead(self):
+        return self.overhead
+
+    def get_algorithm(self):
+        return self.algorithm
+
+    # def get_bbox(self):
+    #     return self.bbox
+    #
+    # def get_key(self):
+    #     return self.key
