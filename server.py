78d65dbf4158ee460dc3c264fdb01cc138dfa837
divyamaiya <dmaiya@umass.edu>
2021-12-05 14:27:29 -0500

    Fix flask server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 # Handle front end requests
 # Instantiate controller
+import os
 
 from flask import Flask, request, render_template
 import json
@@ -8,11 +9,12 @@ import webbrowser
 from controller.controller import Controller
 from model.model import Model
 from utils import map_utils
-from utils.map_utils import get_node_from_address
 
 app = Flask(__name__)
 # graphs = {}
-graph = map_utils.load_graph()
+print(os.getcwd())
+graph = map_utils.load_map()
+
 webbrowser.open('http://localhost:5000', new=2)
 
 
@@ -23,31 +25,34 @@ def index():
 
 @app.route('/route', methods=['POST'])
 def route():
-
     data = json.loads(request.data)
     print(data)
 
-    goal = data['goal']
+    mode = data['mode']
     algo = data['algorithm']
 
     # Get Lat Long of Address from Nominatim Geocoding API
-    try:
-        start_node = get_node_from_address(graph, data['start'])
-        dest_node = get_node_from_address(graph, data['dest'])
-    except Exception as e:
-        print(e)
-        return {'error': str(e)}, 501
+    # try:
+    #     start_node = get_node_from_address(graph, data['start'])
+    #     dest_node = get_node_from_address(graph, data['dest'])
+    # except Exception as e:
+    #     print(e)
+    #     return {'error': str(e)}, 501
 
     limit = float(data['limit']) / 100
 
     model = Model()
-    model.set_mode(goal)
+    model.set_mode(mode)
     model.set_limit(limit)
     model.set_algorithm(algo)
-    model.set_source(start_node)
-    model.set_destination(dest_node)
+    model.set_source(data['start'])
+    model.set_destination(data['dest'])
 
     ctr = Controller()
     ctr.set_model(model)
 
     return ctr.handle_request(graph)
+
+
+if __name__ == "__main__":
+    app.run()
