411a54bf267127a7dc00a7dcc5ec7c9d1f3df037
divyamaiya <dmaiya@umass.edu>
2021-12-06 14:05:15 -0500

    Model unit test

--- test/backend/model/model_test.py
+++ test/backend/model/model_test.py
@@ -1,9 +1,29 @@
 import unittest
 
+from src.backend.model.model import Model
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_model_setters(self):
+        mode = "max"
+        source = "source"
+        dest = "dest"
+        limit = 50
+        algorithm = "AStar"
+
+        model = Model()
+        model.set_mode(mode)
+        model.set_limit(limit)
+        model.set_source(source)
+        model.set_destination(dest)
+        model.set_algorithm(algorithm)
+
+        # Assert all values
+        self.assertEqual(model.get_mode(), mode)
+        self.assertEqual(model.get_limit(), limit)
+        self.assertEqual(model.get_source(), source)
+        self.assertEqual(model.get_algorithm(), algorithm)
+        self.assertEqual(model.get_destination(), dest)
 
 
 if __name__ == '__main__':
