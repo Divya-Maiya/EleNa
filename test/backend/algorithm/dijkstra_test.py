254b70b7140080b0fde3b0d08f676bf0a96fad61
Neha Prakash <neha.prakash98@gmail.com>
2021-12-05 22:18:29 -0500

    Added tests for Dijkstra's algorithm

--- test/backend/algorithm/dijkstra_test.py
+++ test/backend/algorithm/dijkstra_test.py
@@ -1,9 +1,23 @@
 import unittest
+import networkx as nx
+
+from src.backend.algorithm.dijkstra import Dijkstra
+from src.backend.utils.map_utils import load_map
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    graph = None
+    limit = 5
+
+    def test_get_shortest_path(self):
+        graph = load_map("data/graph_Amherst.pkl", changeDir=1)
+        start_node = 0
+        dest_node = 10
+        received_shortest_path = Dijkstra().get_shortest_path(graph, start_node, dest_node)
+        expected_shortest_path = nx.shortest_path(graph, start_node, dest_node, weight='length')
+        self.assertEqual(received_shortest_path, expected_shortest_path)
+
+    # def test_dijkstra(self):
 
 
 if __name__ == '__main__':
