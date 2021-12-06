1e845eec22b35a1e4cd1fec9a10a97fed42971f4
divyamaiya <dmaiya@umass.edu>
2021-12-05 22:28:59 -0500

    Add test for Dijkstra

--- test/backend/algorithm/dijkstra_test.py
+++ test/backend/algorithm/dijkstra_test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import networkx as nx
 
@@ -10,9 +11,11 @@ class MyTestCase(unittest.TestCase):
     limit = 5
 
     def test_get_shortest_path(self):
-        graph = load_map("data/graph_Amherst.pkl", changeDir=1)
-        start_node = 0
-        dest_node = 10
+        print(os.getcwd())
+        os.chdir("../..")
+        graph = load_map("resources/graph_Amherst.pkl", changeDir=1)
+        start_node = 66692331
+        dest_node = 66737929
         received_shortest_path = Dijkstra().get_shortest_path(graph, start_node, dest_node)
         expected_shortest_path = nx.shortest_path(graph, start_node, dest_node, weight='length')
         self.assertEqual(received_shortest_path, expected_shortest_path)
