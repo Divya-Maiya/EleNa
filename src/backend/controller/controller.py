a24e35ed37524e0485f239f1d05cb82d94fd5e5b
divyamaiya <dmaiya@umass.edu>
2021-12-06 14:44:33 -0500

    Add documentation for controller

--- src/backend/controller/controller.py
+++ src/backend/controller/controller.py
@@ -6,6 +6,8 @@ import osmnx as ox
 
 """
 Controller class
+ - Handles the requests received by the server
+ - Has-a model object
 """
 
 
@@ -18,6 +20,17 @@ class Controller(object):
         self.model = model
 
     def get_route(self, graph, source_node, dest_node, algorithm='AStar', limit=0, mode='min', plot_local=0):
+        """
+        Calls the appropriate methods (based on algorithm) to get the route
+        :param graph: graph object
+        :param source_node: node id of source
+        :param dest_node: node id of dest
+        :param algorithm: preferred algorithm
+        :param limit: limit
+        :param mode: mode
+        :param plot_local: whether to plot locally
+        :return: final route information
+        """
         path = ""
         if algorithm == 'AStar':
             astar = AStar()
@@ -33,23 +46,26 @@ class Controller(object):
 
         final_path, path_data = convert_path(graph, path)
 
-        # print(path)
-        print(path_data)
+        # print(path_data)
 
+        # Plot the local graph - for local debugging
         if plot_local == 1:
             ox.plot_graph_route(graph, path)
 
         return {'path': final_path, 'path_data': path_data}
 
     def handle_request(self, graph, plot_local=0):
-        # #Load map
-        # graph = load_map()
-        #
-        #Get lt and long of source and dest
+        """
+        Handle requests received from the server
+        :param graph: graph object
+        :param plot_local: whether to plot the map locally
+        :return: final route information
+        """
+        # Get nodes from graph that are nearest to the given source and destination
         source_node = get_node_from_address(graph, self.model.get_source())
         dest_node = get_node_from_address(graph, self.model.get_destination())
 
-        #Get route
+        # Get route
         return self.get_route(graph, source_node, dest_node,
                               self.model.get_algorithm(),
                               self.model.get_limit(),
